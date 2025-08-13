81cbc5ac8fd0c0574fdefe11ea5ffdcfa26a0203
aakansha <tembhurneaakansha@gmail.com>
2025-08-13 21:32:21 +0530

    Add container with most water

---
+++ Container With Most Water.py
@@ -0,0 +1,21 @@
+def maxArea(height):
+    left = 0
+    right = len(height) - 1
+    max_water = 0
+    
+    while left < right:
+        # Calculate area
+        h = min(height[left], height[right])
+        width = right - left
+        area = h * width
+        
+        # Update max water
+        max_water = max(max_water, area)
+        
+        # Move pointer
+        if height[left] < height[right]:
+            left += 1
+        else:
+            right -= 1
+            
+    return max_water
